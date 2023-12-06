ab669e24c515253d024903059899aa012dbb9299
xone404 <alfidjokorusmono@gmail.com>
2023-12-06 21:04:52 +0700

    first commit

---
+++ app/src/main/java/com/example/recycleview/Data.kt
@@ -0,0 +1,3 @@
+package com.example.recycleview
+
+data class Data(val title : String, val logo : Int)
---
+++ app/src/main/java/com/example/recycleview/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.example.recycleview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import java.util.ArrayList
+import java.util.Locale
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var searchView: SearchView
+    private var mList = ArrayList<Data>()
+    private lateinit var adapter: DataAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.recyclerView)
+        searchView = findViewById(R.id.searchView)
+
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        addDataToList()
+        adapter = DataAdapter(mList)
+        recyclerView.adapter = adapter
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                filterList(newText)
+                return true
+            }
+
+        })
+    }
+
+    private fun filterList(query: String?) {
+
+        if (query != null) {
+            val filteredList = ArrayList<Data>()
+            for (i in mList) {
+                if (i.title.lowercase(Locale.ROOT).contains(query)) {
+                    filteredList.add(i)
+                }
+            }
+
+            if (filteredList.isEmpty()) {
+                Toast.makeText(this, "No Data found", Toast.LENGTH_SHORT).show()
+            } else {
+                adapter.setFilteredList(filteredList)
+            }
+        }
+    }
+
+    private fun addDataToList() {
+        mList.add(Data("Esther Howard", R.drawable.gambar1))
+        mList.add(Data("Ronald Richards", R.drawable.gambar2))
+        mList.add(Data("Guy Hawkins", R.drawable.gambar3))
+        mList.add(Data("Jerome Bell", R.drawable.gambar4))
+        mList.add(Data("Leslie Alexander", R.drawable.gambar5))
+        mList.add(Data("Devon Lane", R.drawable.gambar6))
+        mList.add(Data("Savannah Nguyen", R.drawable.gambar7))
+        mList.add(Data("Jorgeo Ramos", R.drawable.gambar8))
+    }
+}
